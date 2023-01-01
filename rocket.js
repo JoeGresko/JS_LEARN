457719d882cd0782515abaa92cc780c7fd974460
Joe <118204039+JoeGresko@users.noreply.github.com>
2023-01-01 18:40:38 +0200

    Trying functions in JavaScript

---
+++ rocket.js
@@ -0,0 +1,33 @@
+var favColor = 'red';
+var myFavColors = ['blue', 'red', 'green'];
+var numOfFavColors = 3;
+var hasGotFavColors = true;
+var richObject = {
+	FirstName: 'Joe',
+	LastName: 'Gresko',
+	favColors: ['blue', 'red', 'green'],
+	yearsAlive: 120,
+	isMale: true
+
+};
+
+richObject.isMale
+
+function whatIsMyFavColor () {
+
+	return 'blue';
+
+}
+
+	function doSomething () {
+		console.log("do something!!!");
+		
+	}
+
+function area (width, height){
+	return width * height;
+}
+
+function fullname (FirstName, SecondName){
+	return FirstName + ' ' + SecondName;
+}
