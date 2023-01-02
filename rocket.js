085fba0057c5c6cce41115105dced58e1a426d36
Joe <118204039+JoeGresko@users.noreply.github.com>
2023-01-02 11:54:18 +0200

    Wrote some js for the car

--- rocket.js
+++ rocket.js
@@ -1,33 +1,29 @@
-var favColor = 'red';
-var myFavColors = ['blue', 'red', 'green'];
-var numOfFavColors = 3;
-var hasGotFavColors = true;
-var richObject = {
-	FirstName: 'Joe',
-	LastName: 'Gresko',
-	favColors: ['blue', 'red', 'green'],
-	yearsAlive: 120,
-	isMale: true
-
-};
-
-richObject.isMale
-
-function whatIsMyFavColor () {
-
-	return 'blue';
-
-}
-
-	function doSomething () {
-		console.log("do something!!!");
-		
+var car = {
+	make: 'BMV',
+	type: 'Polo',
+	color: 'blue',
+	is_turned_on: false,
+	number_wheels: 4,
+	seats: [
+	'seat1', 
+	'seat2', 
+	'seat3', 
+	'seat4'
+	],
+ignite: function () {
+	this.is_turned_on = true;
+	return('Brrrrrrr');
+	},
+fly: function () {
+	alert ('fly');
+	},
+swap: function (is_on) {
+	console.log('turn car' + is_on)
+	if (is_on == true) {
+		this.is_turned_on = true;
+		} else {
+			this.is_turned_on = false;
+			}
 	}
-
-function area (width, height){
-	return width * height;
-}
-
-function fullname (FirstName, SecondName){
-	return FirstName + ' ' + SecondName;
-}
+};
+	
